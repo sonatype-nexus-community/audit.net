cc720587e7ae11ecf538ab9570c2c21110f8bdf6
Allister Beharry <allister.beharry@gmail.com>
2019-08-05 01:02:58 -0400

    Use correct Lib call in tests.

--- tests/NugetAuditor.Tests/NugetAuditorLibTests.cs
+++ tests/NugetAuditor.Tests/NugetAuditorLibTests.cs
@@ -17,7 +17,7 @@ namespace NugetAuditor.Tests
         {
             var f3 = Path.Combine("TestFiles", "packages.config.example.3");
             Assert.IsTrue(File.Exists(f3));
-            var results = Lib.NugetAuditor.AuditPackages(f3, 0);
+            var results = Lib.NugetAuditor.AuditPackages(f3, 0, new NuGet.Common.NullLogger());
             Assert.AreNotEqual(0, results.Count(r => r.MatchedVulnerabilities > 0));
         }
     }
